b61a5ca4ed7d3bd2473e71fb24619c5405dac6a9
Selaby <oratnek.ihcuekat@gmail.com>
2021-05-19 21:09:29 +0900

    6まで完了

--- search.py
+++ search.py
@@ -2,24 +2,29 @@ import pandas as pd
 import eel
 
 ### デスクトップアプリ作成課題
-def kimetsu_search(word):
+def kimetsu_search(word,csvfile):
     # 検索対象取得
-    df=pd.read_csv("./source.csv")
+    df=pd.read_csv(f"./{csvfile}")
     source=list(df["name"])
 
     # 検索
     if word == "":
-        print("名前を入力してください")
+        # print("名前を入力してください")
+        eel.output("名前を入力してください")
     elif word in source:
-        print("『{}』はあります".format(word))
+        # print("『{}』はあります".format(word))
+        eel.output(f"『{word}』はいます")
     else:
-        print("『{}』はありません".format(word))
+        # print("『{}』はありません".format(word))
+        eel.output(f"『{word}』はいません")
         # 追加
-        #add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
-        #if add_flg=="1":
+        # add_flg=input("追加登録しますか？(0:しない 1:する)　＞＞　")
+        # if add_flg=="1":
         source.append(word)
+        # print("『{}』を追加しました".format(word))
+        eel.output(f"『{word}』を追加しました")
     
     # CSV書き込み
     df=pd.DataFrame(source,columns=["name"])
-    df.to_csv("./source.csv",encoding="utf_8-sig")
-    print(source)
+    df.to_csv(f"./{csvfile}",encoding="utf_8-sig")
+    # print(source)
